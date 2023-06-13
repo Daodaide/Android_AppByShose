91f627eb44c4dc9f339d54d3ba99e234b38b82cd
Lê Ánh Tuyền <tuyenlace150429@fpt.edu.vn>
2023-06-13 22:27:46 +0700

    change main and change password

--- app/src/main/java/com/example/se1603_prm392_shoestoreapp_group05/View/MainActivity.java
+++ app/src/main/java/com/example/se1603_prm392_shoestoreapp_group05/View/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.se1603_prm392_shoestoreapp_group05.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.se1603_prm392_shoestoreapp_group05.R;
 
@@ -12,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnLaunchTestActivity = findViewById(R.id.btn_launch_test_activity);
+        btnLaunchTestActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ForgotPasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
