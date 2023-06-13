eb988a401921458b6bca60f2190909d8430a11ca
Dao <g1trandao@gmail.com>
2023-06-13 22:59:33 +0700

    Sai Deo Sua Nha

--- app/src/main/java/com/example/se1603_prm392_shoestoreapp_group05/View/LoginActivity.java
+++ app/src/main/java/com/example/se1603_prm392_shoestoreapp_group05/View/LoginActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.se1603_prm392_shoestoreapp_group05.R;
+import android.app.Activity;
+import android.os.Bundle;
 
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 public class LoginActivity extends AppCompatActivity {
 
+    private View ic_launcher_background;
+    private ImageView vector;
+    private ImageView image_6;
+    private TextView sing_up;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
