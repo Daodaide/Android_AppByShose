c4fce08192f29214b99ba73f1fabc5938daaf50c
Dao <g1trandao@gmail.com>
2023-07-06 21:44:51 +0700

    Login,Register,sua Home, Chat fix chua xong

--- app/src/main/java/com/example/se1603_prm392_shoestoreapp_group05/View/RegisterActivity.java
+++ app/src/main/java/com/example/se1603_prm392_shoestoreapp_group05/View/RegisterActivity.java
@@ -2,6 +2,7 @@ package com.example.se1603_prm392_shoestoreapp_group05.View;
 
 import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
@@ -45,7 +46,6 @@ public class RegisterActivity extends AppCompatActivity {
         });
 
 
-
         databaseHelper = new DatabaseHelper(this);
 
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -63,32 +63,48 @@ public class RegisterActivity extends AppCompatActivity {
         String email = etEmail.getText().toString().trim();
         String phoneNum = etPhoneNum.getText().toString().trim();
         String address = etAddress.getText().toString().trim();
-        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+
         if (username.isEmpty() || password.isEmpty() || passwordConfirm.isEmpty() || email.isEmpty() || phoneNum.isEmpty() || address.isEmpty()) {
             Toast.makeText(this, "Please fill in all fields", Toast.LENGTH_SHORT).show();
         } else if (!password.equals(passwordConfirm)) {
             Toast.makeText(this, "Passwords do not match", Toast.LENGTH_SHORT).show();
+        } else if (!email.matches(".*@gmail\\.com") || email.length() > 20) {
+            Toast.makeText(this, "Please enter a valid Gmail address (max 20 characters)", Toast.LENGTH_SHORT).show();
+        } else if (phoneNum.length() != 10) {
+            Toast.makeText(this, "Please enter a 10-digit phone number", Toast.LENGTH_SHORT).show();
+        } else if (!phoneNum.matches("\\d+")) {
+            Toast.makeText(this, "Please enter a numeric phone number", Toast.LENGTH_SHORT).show();
         } else {
             SQLiteDatabase db = databaseHelper.getWritableDatabase();
 
-            ContentValues values = new ContentValues();
-            values.put(DatabaseHelper.COLUMN_USERNAME, username);
-            values.put(DatabaseHelper.COLUMN_PASSWORD, password);
-            values.put(DatabaseHelper.COLUMN_EMAIL, email);
-            values.put(DatabaseHelper.COLUMN_PHONE, phoneNum);
-            values.put(DatabaseHelper.COLUMN_ADDRESS, address);
-
-            long result = db.insert(DatabaseHelper.TABLE_USER, null, values);
-
-            if (result == -1) {
-                Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT).show();
+            // Check if the email already exists in the database
+            String query = "SELECT * FROM " + DatabaseHelper.TABLE_USER + " WHERE " + DatabaseHelper.COLUMN_EMAIL + "=?";
+            Cursor cursor = db.rawQuery(query, new String[]{email});
+            if (cursor.moveToFirst()) {
+                // Email already exists
+                Toast.makeText(this, "Email already exists", Toast.LENGTH_SHORT).show();
             } else {
-                Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show();
-                finish();
+                // Email is unique, proceed with registration
+                ContentValues values = new ContentValues();
+                values.put(DatabaseHelper.COLUMN_USERNAME, username);
+                values.put(DatabaseHelper.COLUMN_PASSWORD, password);
+                values.put(DatabaseHelper.COLUMN_EMAIL, email);
+                values.put(DatabaseHelper.COLUMN_PHONE, phoneNum);
+                values.put(DatabaseHelper.COLUMN_ADDRESS, address);
+
+                long result = db.insert(DatabaseHelper.TABLE_USER, null, values);
+
+                if (result == -1) {
+                    Toast.makeText(this, "Failed to register", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
             }
 
+            cursor.close();
             db.close();
         }
     }
